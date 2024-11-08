f25d7b1d59a658245fd28e5fb750f3edce013c03
Passakorn-V <156067673+Passakorn-V@users.noreply.github.com>
2024-11-08 08:13:31 +0700

    add transcript

--- portfolio_new/src/components/header/CTA.jsx
+++ portfolio_new/src/components/header/CTA.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Resume from '../../assets/Resume_Passakorn_Vanchana.pdf'
+import Transcript from '../../assets/Transcript_Passakorn_Vanchana.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
         <a href={Resume} download className='btn'>Resume</a>
         <a href='#contact' className='btn btn-primary'>Connect</a>
+        <a href={Transcript} download className='btn'>Transcript</a>
     </div>
   )
 }
